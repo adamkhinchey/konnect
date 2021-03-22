c0392aad3bf6dcde0b888e618ef357a476bcd0f2
Udit Bhardwaj <udit.bhardwaj@dotsquares.com>
2021-03-22 13:04:49 +0530

    scaffolding: segregates modules (core, features, shared), adds interceptor in core module

---
+++ src/app/core/core.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { CoreRoutingModule } from './core-routing.module';
+import {httpInterceptorProviders} from './http-interceptors';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    CoreRoutingModule
+  ],
+  providers: [
+   httpInterceptorProviders
+  ]
+})
+export class CoreModule { }
