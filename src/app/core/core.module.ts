d06b436625378a9962e6aed7c7a83ca195c0cc2e
udit <udit.bhardwaj@dotsquares.com>
2021-03-22 19:35:48 +0530

    starts working on feature/login

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -5,9 +5,13 @@ import { CoreRoutingModule } from './core-routing.module';
 import {httpInterceptorProviders} from './http-interceptors';
 import { LoginComponent } from './components/login/login.component';
 import { ForgotPasswordComponent } from './components/forgot-password/forgot-password.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { BodyComponent } from './components/body/body.component';
+import { LayoutComponent } from './components/layout/layout.component';
 
 @NgModule({
-  declarations: [LoginComponent, ForgotPasswordComponent],
+  declarations: [LoginComponent, ForgotPasswordComponent, HeaderComponent, FooterComponent, BodyComponent, LayoutComponent],
   imports: [
     CommonModule,
     CoreRoutingModule
