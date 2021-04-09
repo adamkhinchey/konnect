3942f98c18d35b93eba96ed4f37de3069ada6ef1
udit <udit.bhardwaj@dotsquares.com>
2021-04-09 10:22:25 +0530

    prepares for uat upload

--- src/environments/environment.uat.ts
+++ src/environments/environment.uat.ts
@@ -1,3 +1,19 @@
+import timeZones from './timeZones';
+import countries from './countries';
+import companyCategories from './company-categories';
+
+const APIURL = 'https://zfyd7d1tbb.execute-api.ap-southeast-2.amazonaws.com/uat';
+const siteURL = 'http://uat-user-website.s3-website-ap-southeast-2.amazonaws.com';
+
 export const environment = {
-  production: false
+  production: false,
+  env: 'uat',
+  timeZones,
+  countries,
+  companyCategories,
+  apiBaseURL: APIURL,
+  jwtKey: 'tkn',
+  imageFileUploadSize: 5 * 1024 * 1024, // 5MB
+  imageFileAllowedFormats: ['image/jpeg', 'image/png'],
+  siteURL
 };
