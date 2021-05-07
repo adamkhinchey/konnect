92584c685489ac802df3e91cfdda1fd57ebb5009
lalit <lalit.sharma@dotsquares.com>
2021-05-07 17:27:09 +0530

    changes

--- src/app/features/users/components/events-listings/events-listings.component.ts
+++ src/app/features/users/components/events-listings/events-listings.component.ts
@@ -1,16 +1,32 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, AfterViewInit, ViewChild, AfterViewChecked, ChangeDetectorRef } from '@angular/core';
+import { IgxCalendarComponent, DateRangeType, DateRangeDescriptor } from 'igniteui-angular';
 
 @Component({
   selector: 'app-events-listings',
   templateUrl: './events-listings.component.html',
   styleUrls: ['./events-listings.component.scss']
 })
-export class EventsListingsComponent implements OnInit {
+export class EventsListingsComponent implements OnInit, AfterViewChecked {
+  @ViewChild('calendar', { static: true }) calendar: IgxCalendarComponent = new IgxCalendarComponent();
   @Input() events: any;
-  constructor() { }
+  specialDates: DateRangeDescriptor[] = [];
+  constructor(private cdRef: ChangeDetectorRef) { }
 
   ngOnInit(): void {
     console.log(this.events);
   }
+
+  ngAfterViewChecked() {
+    this.specialDates = [{
+      type: DateRangeType.Specific, dateRange: [
+        new Date(2021, 5 - 1, 4),
+        new Date(2021, 5 - 1, 14),
+        new Date(2021, 5 - 1, 15),
+        new Date(2021, 8 - 1, 14)
+      ]
+    }];
+    this.cdRef.detectChanges();
+  }
+
 
 }
