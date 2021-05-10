bc8091e07d877e96b7fecb1b44d86376672ed002
lalit <lalit.sharma@dotsquares.com>
2021-05-10 12:45:32 +0530

    changes

--- src/app/features/users/components/events-listings/events-listings.component.ts
+++ src/app/features/users/components/events-listings/events-listings.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, OnInit, AfterViewInit, ViewChild, AfterViewChecked, ChangeDetectorRef } from '@angular/core';
 import { Router } from '@angular/router';
 import { IgxCalendarComponent, DateRangeType, DateRangeDescriptor } from 'igniteui-angular';
+import * as moment from 'moment'
 
 @Component({
   selector: 'app-events-listings',
@@ -10,6 +11,7 @@ import { IgxCalendarComponent, DateRangeType, DateRangeDescriptor } from 'ignite
 export class EventsListingsComponent implements OnInit, AfterViewChecked {
   @ViewChild('calendar', { static: true }) calendar: IgxCalendarComponent = new IgxCalendarComponent();
   @Input() events: any;
+  dates: any = [];
   specialDates: DateRangeDescriptor[] = [];
   constructor(
     private cdRef: ChangeDetectorRef,
@@ -18,19 +20,55 @@ export class EventsListingsComponent implements OnInit, AfterViewChecked {
 
   ngOnInit(): void {
     console.log(this.events);
-  }
-
-  ngAfterViewChecked() {
+    for (let i = 0; i < this.events.length; i++) {
+      console.log('loop time');
+      console.log(i);
+      var startDate;
+      var endDate;
+      startDate = moment(this.events[i].eventStartDate).format('YYYY-MM-DD');
+      endDate = moment(this.events[i].eventEndDate).format('YYYY-MM-DD');
+      if(moment(startDate).isSame(endDate)){
+        this.dates.push(new Date(startDate));
+      };
+    }
+    console.log(this.dates);
     this.specialDates = [{
-      type: DateRangeType.Specific, dateRange: [
-        new Date(2021, 5 - 1, 4),
-        new Date(2021, 5 - 1, 14),
-        new Date(2021, 5 - 1, 15),
-        new Date(2021, 8 - 1, 14)
-      ]
+      type: DateRangeType.Specific, dateRange: this.dates
+      // [
+      //   new Date(2021, 5 - 1, 4),
+      //   new Date(2021, 5 - 1, 14),
+      //   new Date(2021, 5 - 1, 15),
+      //   new Date(2021, 8 - 1, 14)
+      // ]
     }];
     this.cdRef.detectChanges();
     console.log(this.specialDates);
+  }
+
+  ngAfterViewChecked() {
+    // for (let i = 0; i < this.events.length; i++) {
+    //   console.log('loop time');
+    //   console.log(i);
+    //   var startDate;
+    //   var endDate;
+    //   startDate = moment(this.events[i].eventStartDate).format('YYYY-MM-DD');
+    //   endDate = moment(this.events[i].eventEndDate).format('YYYY-MM-DD');
+    //   if(moment(startDate).isSame(endDate)){
+    //     this.dates.push(new Date(startDate));
+    //   };
+    // }
+    // console.log(this.dates);
+    // this.specialDates = [{
+    //   type: DateRangeType.Specific, dateRange: this.dates
+    //   // [
+    //   //   new Date(2021, 5 - 1, 4),
+    //   //   new Date(2021, 5 - 1, 14),
+    //   //   new Date(2021, 5 - 1, 15),
+    //   //   new Date(2021, 8 - 1, 14)
+    //   // ]
+    // }];
+    // this.cdRef.detectChanges();
+    // console.log(this.specialDates);
   }
 
   createEvent() {
