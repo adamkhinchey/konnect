b99606b4227d7639361edcdb32e46e511750cb98
lalit <lalit.sharma@dotsquares.com>
2021-05-21 10:36:25 +0530

    change api url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,10 +4,10 @@ import companyCategories from './company-categories';
 import eventContactLabels from './event-contact-label-list';
 
 const APIURL = 'https://yxraels57k.execute-api.ap-southeast-2.amazonaws.com/dev';
-// const localProxyURL = 'http://localhost:4202/api';
-// const siteURL = 'http://localhost:4202';
-const localProxyURL = 'http://localhost:3000/dev';
-const siteURL = 'http://localhost:4200';
+ const localProxyURL = 'http://localhost:4202/api';
+ const siteURL = 'http://localhost:4202';
+//const localProxyURL = 'http://localhost:3000/dev';
+//const siteURL = 'http://localhost:4200';
 
 export const environment = {
   production: false,
