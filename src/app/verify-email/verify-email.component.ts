80f82a0aa93adb60e41032f94c70a462146633b3
banwari purvya <banwarilal.purvya@dotsquares.com>
2021-10-14 11:19:58 +0530

    ui changes

--- src/app/verify-email/verify-email.component.ts
+++ src/app/verify-email/verify-email.component.ts
@@ -7,22 +7,22 @@ import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
   styleUrls: ['./verify-email.component.scss']
 })
 export class VerifyEmailComponent implements OnInit {
-  @ViewChild('content', { static: false }) modalContent!: TemplateRef<any>;
-  @ViewChild('contentfailed', { static: false }) modalContentFailed!: TemplateRef<any>;
+  // @ViewChild('content', { static: false }) modalContent!: TemplateRef<any>;
+  // @ViewChild('contentfailed', { static: false }) modalContentFailed!: TemplateRef<any>;
   constructor(
     private modalService: NgbModal,
   ) { }
 
   ngOnInit(): void {
-    this.showSuccessModel();
+    //this.showSuccessModel();
   }
 
-  showSuccessModel(){
-    this.modalService.open(this.modalContent, { centered: true, backdrop: false, keyboard: false });
-  }
+  // showSuccessModel(){
+  //   this.modalService.open(this.modalContent, { centered: true, backdrop: false, keyboard: false });
+  // }
 
-  showErrorModel(){
-    this.modalService.open(this.modalContentFailed, { centered: true, backdrop: false, keyboard: false });
-  }
+  // showErrorModel(){
+  //   this.modalService.open(this.modalContentFailed, { centered: true, backdrop: false, keyboard: false });
+  // }
 
 }
