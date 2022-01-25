b4db39a894958e9fe67ca481ffc42d369cb9d703
dashrath <avaydashrath@gmail.com>
2022-01-25 16:05:16 +0530

    fixed email issue

--- src/app/features/edit-events/components/search-or-invite-function-cmp/search-or-invite-function-cmp.component.ts
+++ src/app/features/edit-events/components/search-or-invite-function-cmp/search-or-invite-function-cmp.component.ts
@@ -14,7 +14,7 @@ import { faAddressCard } from '@fortawesome/free-regular-svg-icons';
   styleUrls: ['./search-or-invite-function-cmp.component.scss']
 })
 export class SearchOrInviteFunctionCmpComponent implements OnInit, OnDestroy {
-  EMAIL_REGEX = new RegExp(/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,3}))$/);
+  EMAIL_REGEX = new RegExp(/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,6}))$/);
   addressCardIcon = faAddressCard;
   @Output() closed = new EventEmitter();
   @Output() newCompanyInvited = new EventEmitter<InviteFnCmpClass>();
