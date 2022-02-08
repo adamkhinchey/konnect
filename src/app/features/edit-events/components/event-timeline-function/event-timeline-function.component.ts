39cb25df901e47a7578394fa2d8f07b24eda6a62
lalit <lalit.sharma@dotsquares.com>
2022-02-08 16:00:54 +0530

    changes

--- src/app/features/edit-events/components/event-timeline-function/event-timeline-function.component.ts
+++ src/app/features/edit-events/components/event-timeline-function/event-timeline-function.component.ts
@@ -1,23 +1,36 @@
-import {AfterViewInit, Component, Input, OnDestroy, OnInit, SimpleChanges, ViewChild} from '@angular/core';
-import {EventGanttChartComponent} from "../event-gantt-chart/event-gantt-chart.component";
-import {EventTimelineService} from "../../services/event-timeline.service";
-import {devLogger} from "../../../../shared/utils";
-import {Subject, Subscription} from "rxjs";
-import {ToastrService} from "ngx-toastr";
-import {EventTimelineDataInterface} from "../../models/interfaces";
-import {EventTimelineType} from "../../../../shared/models";
-import {NgbNavChangeEvent} from "@ng-bootstrap/ng-bootstrap";
+import {
+  AfterViewInit,
+  Component,
+  Input,
+  OnDestroy,
+  OnInit,
+  SimpleChanges,
+  ViewChild,
+} from '@angular/core';
+import { EventGanttChartComponent } from '../event-gantt-chart/event-gantt-chart.component';
+import { EventTimelineService } from '../../services/event-timeline.service';
+import { devLogger } from '../../../../shared/utils';
+import { Subject, Subscription } from 'rxjs';
+import { ToastrService } from 'ngx-toastr';
+import { EventTimelineDataInterface } from '../../models/interfaces';
+import { EventTimelineType } from '../../../../shared/models';
+import { NgbNavChangeEvent } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-event-timeline-function',
   templateUrl: './event-timeline-function.component.html',
-  styleUrls: ['./event-timeline-function.component.scss']
+  styleUrls: ['./event-timeline-function.component.scss'],
 })
-export class EventTimelineFunctionComponent implements OnInit, AfterViewInit, OnDestroy {
-
+export class EventTimelineFunctionComponent
+  implements OnInit, AfterViewInit, OnDestroy
+{
   @Input() eventId: number | null = null;
-  @ViewChild('servicesGanttChart') servicesGanttChart: EventGanttChartComponent | undefined;
-  @ViewChild('exhibitorsGanttChart') exhibitorsGanttChart: EventGanttChartComponent | undefined;
+  @ViewChild('servicesGanttChart') servicesGanttChart:
+    | EventGanttChartComponent
+    | undefined;
+  @ViewChild('exhibitorsGanttChart') exhibitorsGanttChart:
+    | EventGanttChartComponent
+    | undefined;
   activeVenueId: number | undefined;
   serviceTimeLineId = 9;
   exhibitorsTimeLineId = 10;
@@ -25,43 +38,55 @@ export class EventTimelineFunctionComponent implements OnInit, AfterViewInit, On
   venues: { venueId: number; venueName: string }[] = [];
   private fetchEventVenueSubs: Subscription | undefined;
   timelineType: EventTimelineType = EventTimelineType.SERVICES;
-  timelineRenderSubject = new Subject<{ elem: HTMLElement, data: EventTimelineDataInterface | undefined }>();
+  timelineRenderSubject = new Subject<{
+    elem: HTMLElement;
+    data: EventTimelineDataInterface | undefined;
+  }>();
   private eventServTLSubs: Subscription | undefined;
   private eventExhTLSubs: Subscription | undefined;
   private tlineRenderTrigrSubs: Subscription | undefined;
   hasVenues = true;
 
-
   constructor(
     private eventTimelineService: EventTimelineService,
     private toaster: ToastrService
-  ) {
-  }
+  ) {}
 
-  ngOnInit(): void {
-
-  }
+  ngOnInit(): void {}
 
   ngAfterViewInit(): void {
-    this.tlineRenderTrigrSubs = this.eventTimelineService.render.subscribe(() => {
-      this.fetchVenuesData(() => {
-        this.fetchServicesTimelineData((data: EventTimelineDataInterface | undefined) => {
-          this.timelineRenderSubject.next({elem: this.servicesGanttChart?.timelineContainer?.nativeElement, data});
+    this.tlineRenderTrigrSubs = this.eventTimelineService.render.subscribe(
+      () => {
+        this.fetchVenuesData(() => {
+          this.fetchServicesTimelineData(
+            (data: EventTimelineDataInterface | undefined) => {
+              this.timelineRenderSubject.next({
+                elem: this.servicesGanttChart?.timelineContainer?.nativeElement,
+                data,
+              });
+            }
+          );
         });
-      });
-    });
+      }
+    );
   }
 
   onNavChange(event: NgbNavChangeEvent<any>): void {
     if (event.nextId === this.serviceTimeLineId) {
       this.timelineType = EventTimelineType.SERVICES;
       this.fetchServicesTimelineData((data) => {
-        this.timelineRenderSubject.next({elem: this.servicesGanttChart?.timelineContainer?.nativeElement, data});
+        this.timelineRenderSubject.next({
+          elem: this.servicesGanttChart?.timelineContainer?.nativeElement,
+          data,
+        });
       });
     } else if (event.nextId === this.exhibitorsTimeLineId) {
       this.timelineType = EventTimelineType.EXHIBITORS;
       this.fetchExhibitorsTimelineData((data) => {
-        this.timelineRenderSubject.next({elem: this.exhibitorsGanttChart?.timelineContainer?.nativeElement, data});
+        this.timelineRenderSubject.next({
+          elem: this.exhibitorsGanttChart?.timelineContainer?.nativeElement,
+          data,
+        });
       });
     }
   }
@@ -71,53 +96,82 @@ export class EventTimelineFunctionComponent implements OnInit, AfterViewInit, On
       this.fetchEventVenueSubs.unsubscribe();
     }
     if (this.eventId) {
-      this.fetchEventVenueSubs = this.eventTimelineService.fetchEventVenues(this.eventId)
-        .subscribe((value) => {
-          this.venues = value;
-          if (Array.isArray(this.venues) && this.venues.length > 0) {
-            this.activeVenueId = value[0].venueId;
-            callback.call(this);
-          } else {
-            this.hasVenues = false;
+      this.fetchEventVenueSubs = this.eventTimelineService
+        .fetchEventVenues(this.eventId)
+        .subscribe(
+          (value) => {
+            this.venues = value;
+            if (Array.isArray(this.venues) && this.venues.length > 0) {
+              this.activeVenueId = value[0].venueId;
+              callback.call(this);
+            } else {
+              this.hasVenues = false;
+            }
+          },
+          (err) => {
+            this.toaster.error(
+              'Failed to load venue data for timeline',
+              'Event Timeline'
+            );
+            devLogger('error', { err });
           }
-        }, (err) => {
-          this.toaster.error('Failed to load venue data for timeline', 'Event Timeline');
-          devLogger('error', {err});
-        });
+        );
     } else {
       this.toaster.error('Invalid event id', 'Event Timeline');
     }
   }
 
-  fetchServicesTimelineData(callback: (data: EventTimelineDataInterface | undefined) => void): void {
+  fetchServicesTimelineData(
+    callback: (data: EventTimelineDataInterface | undefined) => void
+  ): void {
     this.eventServTLSubs?.unsubscribe();
-    this.eventServTLSubs = this.eventTimelineService.fetchServicesTimelineData(this.eventId, this.activeVenueId)
-      .subscribe((value) => {
-        this.timelineData = value.data;
-        devLogger('log', {timelineData: this.timelineData});
-        callback.call(this, this.timelineData);
-      }, (err) => {
-        devLogger('error', {err});
-        this.toaster.error('Failed to load venue services data for timeline', 'Event Timeline: Services');
-      });
+    this.eventServTLSubs = this.eventTimelineService
+      .fetchServicesTimelineData(this.eventId, this.activeVenueId)
+      .subscribe(
+        (value) => {
+          this.timelineData = value.data;
+          devLogger('log', { timelineData: this.timelineData });
+          callback.call(this, this.timelineData);
+        },
+        (err) => {
+          devLogger('error', { err });
+          this.toaster.error(
+            'Failed to load venue services data for timeline',
+            'Event Timeline: Services'
+          );
+        }
+      );
   }
 
-  fetchExhibitorsTimelineData(callback: (data: EventTimelineDataInterface | undefined) => void): void {
+  fetchExhibitorsTimelineData(
+    callback: (data: EventTimelineDataInterface | undefined) => void
+  ): void {
     this.eventExhTLSubs?.unsubscribe();
-    this.eventExhTLSubs = this.eventTimelineService.fetchExhibitorsTimelineData(this.eventId, this.activeVenueId)
-      .subscribe((value) => {
-        this.timelineData = value.data;
-        callback.call(this, this.timelineData);
-      }, (err) => {
-        devLogger('error', {err});
-        this.toaster.error('Failed to load venue exhibitors data for timeline', 'Event Timeline: Exhibitors');
-      });
+    this.eventExhTLSubs = this.eventTimelineService
+      .fetchExhibitorsTimelineData(this.eventId, this.activeVenueId)
+      .subscribe(
+        (value) => {
+          this.timelineData = value.data;
+          callback.call(this, this.timelineData);
+        },
+        (err) => {
+          devLogger('error', { err });
+          this.toaster.error(
+            'Failed to load venue exhibitors data for timeline',
+            'Event Timeline: Exhibitors'
+          );
+        }
+      );
   }
 
   setVenueId(event: any): void {
+    this.timelineType = EventTimelineType.SERVICES;
     this.activeVenueId = event;
     this.fetchServicesTimelineData((data) => {
-      this.timelineRenderSubject.next({elem: this.servicesGanttChart?.timelineContainer?.nativeElement, data});
+      this.timelineRenderSubject.next({
+        elem: this.servicesGanttChart?.timelineContainer?.nativeElement,
+        data,
+      });
     });
   }
 
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,8 @@ import countries from './countries';
 import companyCategories from './company-categories';
 import eventContactLabels from './event-contact-label-list';
 
-// const APIURL = 'https://zfyd7d1tbb.execute-api.ap-southeast-2.amazonaws.com/dev';
-const APIURL = 'http://127.0.0.1:3000/dev';
+ const APIURL = 'https://zfyd7d1tbb.execute-api.ap-southeast-2.amazonaws.com/uat';
+//const APIURL = 'http://127.0.0.1:3000/dev';
 const siteURL = 'http://dev-user-website.s3-website-ap-southeast-2.amazonaws.com';
 
 
