cb65ff9cd6520ab782fcdffe8f45591a7cb23675
banwari purvya <banwarilal.purvya@dotsquares.com>
2022-05-19 12:12:29 +0530

    ui changes

--- src/app/shared/components/add-task/add-task.component.ts
+++ src/app/shared/components/add-task/add-task.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { IDropdownSettings } from 'ng-multiselect-dropdown';
 
 @Component({
   selector: 'app-add-task',
@@ -8,11 +9,44 @@ import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 })
 export class AddTaskComponent implements OnInit {
 
+  dropdownList:any = [];
+  selectedItems:any = [];
+  dropdownSettings:IDropdownSettings = {};
+  
   constructor(
     private activeModal: NgbActiveModal
   ) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.dropdownList = [
+      { item_id: 1, item_text: 'Mumbai' },
+      { item_id: 2, item_text: 'Bangaluru' },
+      { item_id: 3, item_text: 'Pune' },
+      { item_id: 4, item_text: 'Navsari' },
+      { item_id: 5, item_text: 'New Delhi' },
+      { item_id: 6, item_text: 'New Delhi' },
+      { item_id: 7, item_text: 'New Delhi' },
+      { item_id: 8, item_text: 'New Delhi' },
+      { item_id: 9, item_text: 'New Delhi' }
+    ];
+    this.selectedItems = [
+      { item_id: 3, item_text: 'Pune' },
+      { item_id: 4, item_text: 'Navsari' }
+    ];
+    this.dropdownSettings = {
+      singleSelection: false,
+      idField: 'item_id',
+      textField: 'item_text',
+      selectAllText: 'Select All',
+      unSelectAllText: 'UnSelect All',
+      // itemsShowLimit: false,
+      allowSearchFilter: true
+    };
+    
+  }
+  onItemSelect(item: any) {
+    console.log(item);
+  }
 
   public dismiss() {
     this.activeModal.dismiss();
