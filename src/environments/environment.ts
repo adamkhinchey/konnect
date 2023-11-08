d3731b417418e3d7ec72deef889285238716ace2
Rahul Saini <saini.rahul@dotsquares.com>
2023-11-08 16:24:18 +0530

    sprint -5 feedback

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,13 +3,14 @@ import countries from './countries';
 import companyCategories from './company-categories';
 import eventContactLabels from './event-contact-label-list';
 
-const APIURL = 'https://zfyd7d1tbb.execute-api.ap-southeast-2.amazonaws.com/uat';
-const siteURL = 'http://uat-user-website.s3-website-ap-southeast-2.amazonaws.com';
-const importLambdaUrl = 'https://6fwultw46lxcz6vzrih5pf4qjq0sqhoy.lambda-url.ap-southeast-2.on.aws/';
+const APIURL = 'http://192.168.10.92:3005/uat';
+// const APIURL = 'https://yxraels57k.execute-api.ap-southeast-2.amazonaws.com/dev';
+const siteURL = 'http://dev-user-website.s3-website-ap-southeast-2.amazonaws.com';
+const importLambdaUrl = 'https://gds6ucmu4dyexvfrtynp3gw25q0nbckx.lambda-url.ap-southeast-2.on.aws/';
 
 export const environment = {
   production: false,
-  env: 'uat',
+  env: 'development',
   timeZones,
   countries,
   companyCategories,
@@ -41,5 +42,5 @@ export const environment = {
   siteURL,
   importLambdaUrl,
   eventContactLabels,
-  headerName:'Konnect UAT'
+  headerName:'Konnect'
 };
